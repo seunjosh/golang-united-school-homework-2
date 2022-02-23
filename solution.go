bac43fdf9b1b0d9e0fe0931f7a063a3ec4ccd7a0
Joshua <seunaji2015@gmail.com>
2022-02-23 23:24:49 +0100

    Update solution.go

--- solution.go
+++ solution.go
@@ -8,33 +8,30 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-import (
-	"math"
-)
+import "math"
 
 
-
-type sideCircle struct {
-	SidesCircle float64
-}
-
-type sidesTriangle struct {
-	SidesTriangle float64
-}
-
-type sidesSquare struct {
-	SidesSquare float64
-}
+type SidesCount int
 
 const (
-	SidesTriangle = 3
-	SidesCircle   = 0
-	SidesSquare   = 4
+	SidesTriangle SidesCount = 3
+	SidesCircle   SidesCount = 0
+	SidesSquare   SidesCount = 4
 )
 
-func CalcSquare(sideLen float64, sideNum sideCircle) float64 {
-    sideLen = math.Pi
-	return sideLen * sideLen * sideNum.SidesCircle
-    	
-}
+func CalcSquare(sideLen float64, sidesNum SidesCount) float64 {
 
+	if sidesNum == SidesCircle {
+		return math.Pi * sideLen * sideLen
+	}
+
+	if sidesNum == SidesTriangle {
+		return (math.Sqrt(3) / 4) * sideLen * sideLen
+	}
+
+	if sidesNum == SidesSquare {
+		return sideLen * sideLen
+	}
+
+	return 0
+}
